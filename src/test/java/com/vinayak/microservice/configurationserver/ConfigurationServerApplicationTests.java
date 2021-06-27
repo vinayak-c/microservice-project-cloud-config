34d930d5646b651b3fee97a38ab6446f83dbb754
Vinayak <vinayakchaurasia99@gmail.com>
2021-06-27 23:26:49 +0530

    1->Initial commit 2->Added order and product service config 3->Added remote repo config

---
+++ src/test/java/com/vinayak/microservice/configurationserver/ConfigurationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vinayak.microservice.configurationserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigurationServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
